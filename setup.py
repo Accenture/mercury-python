264a10e8f6442548d13a129f6b429227c0b73722
Eric Law <eric.cw.law@gmail.com>
2020-01-10 15:51:45 -0800

    v1.12.29

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='mercury',
-      version='1.12.26',
+      version='1.12.29',
       description='Python Language pack for Mercury',
       author='Eric Law',
       author_email='eric.law@accenture.com',
